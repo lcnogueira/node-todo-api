7822a727d98c15d5b6c292282c86b5b5a387b5e8
Luiz Claudio <luizcns18@gmail.com>
2018-03-05 13:38:38 -0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -10,25 +11,28 @@ const app = express();
 //Midleware
 app.use(bodyParser.json());
 
-//Handlers
+//Todo Handlers
 app.post('/todos', (req,res) => {
-  const todo = new Todo({
-    text: req.body.text
-  });
-
-  todo.save().then(doc => {
-    res.send(doc);
-  }, e => {
-    res.status(400).send(e);
-  });
+  const todo = new Todo({ text: req.body.text });
+  todo.save().then(doc => res.send(doc), e => res.status(400).send(e));
 });
 
 app.get('/todos', (req, res) => {
-  Todo.find().then(todos => {
-    res.send({todos});
-  }, e => {
-    res.status(400).send(e);
-  });
+  Todo.find().then(todos => res.send({todos}), e => res.status(400).send(e));
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then(todo => {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }, e => res.status(400).send(e));
 });
 
 app.listen(3000, () => {
