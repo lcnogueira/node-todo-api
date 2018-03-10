5e3e5d7f5fbfe4650885c071a66b3b78e29561a1
Luiz Claudio <luizcns18@gmail.com>
2018-03-10 00:25:59 -0300

    Add tests for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,24 +4,12 @@ const {ObjectID} = require('mongodb');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
+const { User } = require('../models/user');
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
-//dummy todos
-const dummyTodos = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
-},{
-  _id: new ObjectID(),
-  text: 'Second test todo',
-  completed: true,
-  completedAt: 333
-}];
-
+beforeEach(populateUsers);
 //test lifecicle method
-beforeEach(done => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(dummyTodos);
-  }).then(() => done());
-});
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   //Test if it is correct when passing valid data
@@ -87,10 +75,10 @@ describe('GET /todos/:id', () => {
   
   it('should return todo doc', (done) => {
     request(app)
-      .get(`/todos/${dummyTodos[0]._id.toHexString()}`)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
       .expect(200)
       .expect(res => {
-        expect(res.body.todo.text).toBe(dummyTodos[0].text);
+        expect(res.body.todo.text).toBe(todos[0].text);
       })
       .end(done);
   });
@@ -114,7 +102,7 @@ describe('GET /todos/:id', () => {
 describe('DELETE /todos/:id', () => {
 
   it('should remove a todo', done => {
-    const hexId = dummyTodos[1]._id.toHexString();
+    const hexId = todos[1]._id.toHexString();
 
     request(app)
       .delete(`/todos/${hexId}`)
@@ -153,7 +141,7 @@ describe('DELETE /todos/:id', () => {
 describe('PATCH /todos/:id', () => {
 
   it('should update the todo', (done) => {
-    const hexId = dummyTodos[0]._id.toHexString();
+    const hexId = todos[0]._id.toHexString();
     const text = 'This should be the new text';
 
     request(app)
@@ -172,7 +160,7 @@ describe('PATCH /todos/:id', () => {
   });
 
   it('should clear completedAt when todo is not completed', (done) => {
-    const hexId = dummyTodos[1]._id.toHexString();
+    const hexId = todos[1]._id.toHexString();
     const text = 'This should be the new text!!!';
 
     request(app)
@@ -190,4 +178,81 @@ describe('PATCH /todos/:id', () => {
       .end(done)
 
   });
-})
+});
+
+describe('GET /users/me', () => {
+
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect(res => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect(res => {
+        expect(res.body).toEqual({})
+      })
+      .end(done);
+  });
+});
+
+describe('POST /users', () => {
+
+  it('should create a user', (done) => {
+    const email = 'example@example.com';
+    const password = '123mnb!';
+
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(200)
+      .expect(res => {
+        expect(res.headers['x-auth']).toExist();
+        expect(res.body._id).toExist();
+        expect(res.body.email).toBe(email);
+      })
+      .end(err => {
+        if(err){
+          return done(err);
+        }
+
+        User.findOne({email}).then(user => {
+          expect(user).toExist();
+          expect(user.password).toNotBe(password);
+          done();
+        });
+      });
+  });
+
+  it('should return validation erros if request invalid', (done) => {
+    request(app)
+      .post('/users')
+      .send({
+        email: 'and',
+        password: '123'
+      })
+      .expect(400)
+      .end(done);
+  });
+
+  it('should not create user if email in use', (done) => {
+    request(app)
+      .post('/users')
+      .send({
+        email: users[0].email,
+        password: 'Password123!'
+      })
+      .expect(400)
+      .end(done);
+  });
+
+});
