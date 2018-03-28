38ce3c73b0dfb87a39f2604a22cb354e40dfd582
Luiz Claudio <luizcns18@gmail.com>
2018-03-28 17:57:20 -0300

    Turno some ES5 code into ES6 code

--- server/server.js
+++ server/server.js
@@ -55,23 +55,48 @@ app.get('/todos/:id', authenticate, (req, res) => {
   }, e => res.status(400).send(e));
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
+//ES5
+// app.delete('/todos/:id', authenticate, (req, res) => {
+//   const id = req.params.id;
+
+//   if(!ObjectId.isValid(id)){
+//     return res.status(404).send();
+//   }
+
+//   Todo.findOneAndRemove({
+//     _id: id,
+//     _creator: req.user._id
+//   }).then(todo => {
+//     if(!todo){
+//       return res.status(404).send();
+//     }
+
+//     res.send({todo});
+//   }, e => res.status(400).send(e));
+// });
+
+//ES6
+app.delete('/todos/:id', authenticate, async (req, res) => {
   const id = req.params.id;
 
   if(!ObjectId.isValid(id)){
     return res.status(404).send();
   }
 
-  Todo.findOneAndRemove({
-    _id: id,
-    _creator: req.user._id
-  }).then(todo => {
+  try{
+    const todo = await Todo.findOneAndRemove({
+      _id: id,
+      _creator: req.user._id
+    });
+    
     if(!todo){
       return res.status(404).send();
     }
-
     res.send({todo});
-  }, e => res.status(400).send(e));
+
+  }catch(e){
+    res.status(400).send(e);
+  }
 });
 
 app.patch('/todos/:id', authenticate, (req, res) => {
@@ -101,16 +126,31 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 
 //User Handlers
 //Create user
-app.post('/users', (req,res) => {
-  //It avoids the user is able to update all the obj properties
-  const body = _.pick(req.body, ['email', 'password']);
-  const user = new User(body);
+//ES5
+// app.post('/users', (req, res) => {
+//   //It avoids the user is able to update all the obj properties
+//   const body = _.pick(req.body, ['email', 'password']);
+//   const user = new User(body);
   
-  user.save().then(() => {
-    return user.generateAuthToken();
-  }).then(token => {
+//   user.save().then(() => {
+//     return user.generateAuthToken();
+//   }).then(token => {
+//     res.header('x-auth',token).send(user);
+//   }).catch(e => res.status(400).send(e) );
+// });
+
+//ES6
+app.post('/users', async (req,res) => {
+  //It avoids the user is able to update all the obj properties
+  try{
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+    await user.save();
+    const token = await user.generateAuthToken();
     res.header('x-auth',token).send(user);
-  }).catch(e => res.status(400).send(e) );
+  } catch(e){
+    res.status(400).send(e);
+  }
 });
 
 app.get('/users/me', authenticate, (req, res) => {
@@ -119,23 +159,47 @@ app.get('/users/me', authenticate, (req, res) => {
 
 
 //Login
-app.post('/users/login', (req, res) => {
-  const body = _.pick(req.body, ['email', 'password']);
+//ES5
+// app.post('/users/login', (req, res) => {
+//   const body = _.pick(req.body, ['email', 'password']);
 
-  User.findByCredentials(body.email, body.password).then(user => {
-    return user.generateAuthToken().then(token => {
-      res.header('x-auth',token).send(user);
-    })
-  }).catch(e => {
+//   User.findByCredentials(body.email, body.password).then(user => {
+//     return user.generateAuthToken().then(token => {
+//       res.header('x-auth',token).send(user);
+//     })
+//   }).catch(e => {
+//     res.status(400).send();
+//   });
+// });
+
+//ES6
+app.post('/users/login', async (req, res) => {
+  try{
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = await User.findByCredentials(body.email, body.password);
+    const token = await user.generateAuthToken();
+    res.header('x-auth',token).send(user);
+  } catch (e) {
     res.status(400).send();
-  });
+  }
 });
 
 //Logout
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token).then(() => {
+//ES5
+// app.delete('/users/me/token', authenticate, (req, res) => {
+//   req.user.removeToken(req.token).then(() => {
+//     res.status(200).send();
+//   }), () => {
+//     res.status(400).send();
+//   }
+// });
+
+//ES6
+app.delete('/users/me/token', authenticate, async (req, res) => {
+  try{
+    await req.user.removeToken(req.token);
     res.status(200).send();
-  }), () => {
+  } catch (e) {
     res.status(400).send();
   }
 });
